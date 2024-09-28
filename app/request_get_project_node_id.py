f2798662c9fe1165283aab517ee7a3b84397b023
Nguyen Duy <duy.nguyenhoang@galaxyfinx.com>
2024-09-28 08:51:18 +0700

    Update action

--- app/request_get_project_node_id.py
+++ app/request_get_project_node_id.py
@@ -6,21 +6,20 @@ def get_project_node_id_graphql() -> str:
     query = f'query{{organization(login:"{REPOSITORY_OWNER}") {{projectV2(number: {PROJECT_ID}){{id}}}}}}'
     response = graphql_request('POST', query)
 
-    match response.status_code:
-        case 200:
-            response_data = response.json()
-            try:
-                project_data = response_data['data']['organization']['projectV2']
-                if project_data is None:
-                    raise Exception('Project not found')
-                node_id = project_data['id']
-            except KeyError as exc:
-                raise Exception(
-                    f'Could not obtain the project node id for project {PROJECT_ID}.\n'
-                    f'Exception: {exc}\n'
-                    f'Response: {response_data}'
-                )
-        case status_code:
-            raise Exception(f'Could not obtain the project node id. Status code: {status_code}')
+    if response.status_code == 200:
+        response_data = response.json()
+        try:
+            project_data = response_data['data'].get('organization', {}).get('projectV2', None)
+            if project_data is None:
+                raise Exception('Project not found')
+            node_id = project_data['id']
+        except KeyError as exc:
+            raise Exception(
+                f'Could not obtain the project node id for project {PROJECT_ID}.\n'
+                f'Exception: {exc}\n'
+                f'Response: {response_data}'
+            )
+    else:
+        raise Exception(f'Could not obtain the project node id. Status code: {response.status_code}')
 
     return node_id
